29ef2b4927139ded09c2f9e70948204170ea5597
jasonemarkson <jasonmarkson76@gmail.com>
2021-11-19 09:46:49 -0500

    practiced this module again for additional help

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,31 +1,49 @@
 // your Bomb code here!
 import React, { Component } from 'react'; 
 
+// export default class Bomb extends Component {
+    
+//     constructor(props) {
+//         super()
+//         this.state = {
+//             secondsLeft: props.initialCount
+//         }
+//     }
+    
+//     initialCount = () => {
+//         if (this.state.secondsLeft === 0) {
+//             return "Boom!"
+//         } else {
+//             return `${this.state.secondsLeft} seconds left before I go boom!`
+//         }
+//     }
+
+//     render() {
+//         // const seconds = this.state.secondsLeft === 0 ? "Boom" : `${this.state.secondsLeft} seconds left before I go boom!`
+
+//         return(
+//             <div>
+//                 <h3>{this.initialCount()}</h3>
+//                 <p>{this.props.type}</p>
+
+//             </div>
+//         )
+//     }
+// }
 export default class Bomb extends Component {
-    
-    constructor(props) {
-        super()
-        this.state = {
-            secondsLeft: props.initialCount
-        }
-    }
-    
-    initialCount = () => {
-        if (this.state.secondsLeft === 0) {
-            return "Boom!"
-        } else {
-            return `${this.state.secondsLeft} seconds left before I go boom!`
-        }
+    state = {
+        secondsLeft: this.props.initialCount
     }
 
     render() {
-        // const seconds = this.state.secondsLeft === 0 ? "Boom" : `${this.state.secondsLeft} seconds left before I go boom!`
-
         return(
             <div>
-                <h3>{this.initialCount()}</h3>
-                <p>{this.props.type}</p>
-
+                {
+                this.state.secondsLeft !== 0 ?
+                <div>{this.state.secondsLeft} seconds left before I go boom!</div> :
+                <div>Boom!</div>
+                }
+            
             </div>
         )
     }
